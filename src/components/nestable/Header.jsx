7410420a007a4779159ab99becc7d0e6af05f04f
Nora Velde <noravelde@gmail.com>
2024-09-09 22:11:24 +0200

    fix

--- src/components/nestable/Header.jsx
+++ src/components/nestable/Header.jsx
@@ -15,7 +15,8 @@ export default function Header({ blok }) {
       <nav>
         <ul className="flex justify-between">
           {headerBlok?.nav?.map((item) => {
-            if (item.component === 'link') return <li key={item._uid}>{item.label}</li>
+            if (item.component === 'link') 
+              return  <Link href={`${item.link.cached_url}`} ><li key={item._uid}>{item.label}</li></Link>
             else if (item.component === 'link_group') {
               return (
                 <li key={item._uid}>
