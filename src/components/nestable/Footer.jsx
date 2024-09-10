8fde5a9cb0fbe9d99268f0f52e9ca48ffbbea8e7
Oa Blom <oablom@gmail.com>
2024-09-10 15:33:45 +0200

    Refactor Footer and SignupNewsletter components

--- src/components/nestable/Footer.jsx
+++ src/components/nestable/Footer.jsx
@@ -1,22 +1,26 @@
-import { StoryblokComponent } from '@storyblok/react';
-import LinkGroup from './LinkGroup';
-import SignupNewsletter from './SignupNewsletter';
+import { StoryblokComponent } from "@storyblok/react";
+import LinkGroup from "./LinkGroup";
+import SignupNewsletter from "./SignupNewsletter";
 export default function Footer({ config }) {
-  const footerBlok = config?.find((blok) => blok.component === 'footer');
+  const footerBlok = config?.find((blok) => blok.component === "footer");
   const footerLinkGroups = footerBlok?.body.filter(
-    (item) => item.component === 'link_group'
+    (item) => item.component === "link_group"
   );
   const footerSignUp = footerBlok?.body.find(
-    (item) => item.component === 'signup_newsletter'
+    (item) => item.component === "signup_newsletter"
   );
 
   return (
     <footer className="border lg:h-80 text-black bg-white">
       <nav className="lg:flex justify-between mt-10">
-        <SignupNewsletter blok={footerSignUp} />
-        {footerLinkGroups?.map((item) => (
-          <LinkGroup key={item._uid} blok={item} />
-        ))}
+        <div className="min-w-[400px]">
+          <SignupNewsletter blok={footerSignUp} />
+        </div>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:flex lg:space-x-6 mt-6 lg:mt-0 lg:w-[60%]">
+          {footerLinkGroups?.map((item) => (
+            <LinkGroup key={item._uid} blok={item} />
+          ))}
+        </div>
       </nav>
     </footer>
   );
--- src/components/nestable/SignupNewsletter.jsx
+++ src/components/nestable/SignupNewsletter.jsx
@@ -1,23 +1,26 @@
-'use client';
-import RichTextDefault from './RichText';
+"use client";
+import RichTextDefault from "./RichText";
 
 export default function SignupNewsletter({ blok }) {
-  console.log('asdasdasd', blok);
+  console.log("asdasdasd", blok);
 
   return (
-    <div className="lg:w-[30%] ml-14">
-      <div className="w-[60%]">
+    <div className="lg:ml-14 flex flex-col items-center">
+      {/* RichTextDefault centreras nu korrekt */}
+      <div className="max-w-[400px] flex justify-center">
         <RichTextDefault blok={blok} />
       </div>
-      <form className="flex flex-col mt-8">
-        {/* Wrapper for input and button */}
-        <div className="lg:flex border-2 border-black w-[60%] lg:w-[80%] justify-between">
+      {/* Input och knapp, även dessa centrerade */}
+      <form className=" flex mt-4">
+        <div className="flex border-2 border-black">
           <input
             type="email"
-            placeholder={'     ' + blok.input_label || 'Enter your email'}
-            className="lg:border-none focus:outline-none focus:border-none lg:w-[70%] w-full"
+            placeholder={"     " + blok.input_label || "Enter your email"}
+            className="lg:border-none focus:outline-none focus:border-none lg:w-[70%] w-full p-2"
           />
-          <button className=" text-black p-2 font-bold">Sign Up</button>
+          <button className="text-black p-2 font-bold whitespace-nowrap">
+            Sign Up
+          </button>
         </div>
       </form>
     </div>
